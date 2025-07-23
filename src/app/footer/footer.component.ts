cbd1a0219ed8532fd6e43d61ff6928b3378816c3
Devanshu <devgoyal952@gmail.com>
2025-07-23 18:27:04 +0530

    developed footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,9 +1,11 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { ButtonModule } from "primeng/button";
 import { TableModule } from "primeng/table";
 import { InputGroupModule } from "primeng/inputgroup";
 import { FormsModule } from "@angular/forms";
 import { NgIf } from "@angular/common";
+import { ToastModule } from "primeng/toast";
+import { MessageService } from "primeng/api";
 
 interface NewItem {
   id: number;
@@ -15,12 +17,24 @@ interface NewItem {
 @Component({
   selector: "app-footer",
   standalone: true,
-  imports: [ButtonModule, TableModule, InputGroupModule, FormsModule, NgIf],
+  imports: [
+    ButtonModule,
+    TableModule,
+    InputGroupModule,
+    FormsModule,
+    NgIf,
+    ToastModule,
+  ],
   templateUrl: "./footer.component.html",
   styleUrl: "./footer.component.css",
+  providers: [MessageService],
 })
 export class FooterComponent {
+  showWarning: boolean = false;
   showInputGroup: boolean = false;
+
+  constructor(public messageService: MessageService) {}
+
   newItem: NewItem = {
     id: Math.floor(Math.random() * 1000),
     fruits: null,
@@ -43,10 +57,41 @@ export class FooterComponent {
     },
   ];
 
-  AddItem() {
-    if (this.newItem) {
-      this.products.push({ ...this.newItem });
+  ngOnInit() {
+    const storedProducts = localStorage.getItem("products");
+    if (storedProducts) {
+      this.products = JSON.parse(storedProducts);
+    } else {
+      localStorage.setItem("products", JSON.stringify(this.products));
+    }
+  }
 
+  showToaster(severity: string, summary: string, detail: string) {
+    this.messageService.add({
+      severity: severity,
+      summary: summary,
+      detail: detail,
+    });
+  }
+
+  AddItem() {
+    if (
+      this.newItem.fruits == 0 &&
+      this.newItem.drinks == 0 &&
+      this.newItem.vegetable == 0
+    ) {
+      this.showToaster("warn", "WARN", "The value must be greater than 0");
+      return;
+    } else if (
+      this.newItem.fruits !== null &&
+      this.newItem.drinks !== null &&
+      this.newItem.vegetable !== null
+    ) {
+      this.products.push({ ...this.newItem });
+      this.showToaster("success", "Success", "Successfully Added");
+      localStorage.setItem("products", JSON.stringify(this.products));
+
+      this.showInputGroup = false;
       this.newItem = {
         id: Math.floor(Math.random() * 1000),
         fruits: null,
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-header',
+  selector: "app-header",
   standalone: true,
   imports: [],
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  templateUrl: "./header.component.html",
+  styleUrl: "./header.component.css",
 })
 export class HeaderComponent {
-
+  // ngOnInit(): void {
+  //   const storedProducts = localStorage.getItem("products");
+  //   const products = storedProducts ? JSON.parse(storedProducts) : [];
+  //   this.totalFruits = products.reduce((sum, p) => sum + (p.fruits || 0), 0);
+  //   this.totalVegetables = products.reduce(
+  //     (sum, p) => sum + (p.vegetable || 0),
+  //     0
+  //   );
+  // }
 }
